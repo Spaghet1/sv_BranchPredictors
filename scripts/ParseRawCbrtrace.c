8018ae0a9e15877bf03b49abfd0f5b0541b28117
Ian Ramin <ian@raminsona@gmail.com>
2026-09-26 02:24:50 -0700

    script to get branch traces and parse them to simpler format

--- scripts/ParseRawCbrtrace.c
+++ scripts/ParseRawCbrtrace.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void usageError() {
 	fprintf(stderr, "Usage:\n<exec> <cbrtrace.log> <dest.log>\n");
@@ -11,19 +12,47 @@ void logError(char* path) {
 	exit(EXIT_FAILURE);
 }
 
-int main(int argc, char** argv) {
-	if (argc != 3) usageError();
-	unsigned long long branchAddr, takenAddr, actualAddr;
+void trimNewline(char* line) {
+	int index = strlen(line) - 1;
+	if (line[index] == '\n') line[index] = '\0';
+}
+
+void parseLog(char* rawPath, char* destPath) {
+	FILE* raw = fopen(rawPath, "r");
+	if (raw == NULL) logError(rawPath);
+	FILE* dest = fopen(destPath, "w");
 	char buffer[256];
-	FILE* raw = fopen(argv[1], "r");
-	if (raw == NULL) logError(argv[1]);
-	FILE* dest = fopen(argv[2], "w");
+	unsigned long long branchAddr, takenAddr, actualAddr;
 	while (fgets(buffer, sizeof(buffer), raw)) {
 		sscanf(buffer, "%*llx [%llx, %*llx, %llx] => %llx", &branchAddr, &takenAddr, &actualAddr);
 		fprintf(dest, "0x%.16lX %b\n", branchAddr, takenAddr == actualAddr ? 1 : 0);
 	}
 	fclose(raw);
 	fclose(dest);
-	printf("%s parsed to %s\n", argv[1], argv[2]);
+	printf("%s parsed to %s\n", rawPath, destPath);
+}
+
+void parseStdin() {
+	char rawBuffer[256];
+	char destBuffer[256];
+	while (fgets(rawBuffer, sizeof(rawBuffer), stdin)) {
+		// printf("%s\n", rawBuffer);
+		trimNewline(rawBuffer);
+		strcpy(destBuffer, rawBuffer);
+		strcat(destBuffer, ".bp");
+		parseLog(rawBuffer, destBuffer);
+	}
+}
+
+int main(int argc, char** argv) {
+	if (argc == 1) parseStdin();
+	else if (argc == 2) {
+		char buffer[256];
+		strcpy(buffer, argv[1]);
+		strcat(buffer, ".bp");
+		parseLog(argv[1], buffer);
+	}
+	else if (argc == 3) parseLog(argv[1], argv[2]);
+	else usageError();
 	return 0;
 }
---
+++ scripts/cbrtraceMacro.sh
@@ -0,0 +1,3 @@
+# script to work around evil dynamoRIO directory structure expectations
+
+./DynamoRIO-Linux-11.91.20715/bin64/drrun -c libcbrtrace.so -- "$@"
